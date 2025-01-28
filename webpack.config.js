fda541da2375eec971ff2b6e45a000a0500ebd2f
mjrivera682 <mjrivera682@gmail.com>
2025-01-28 17:20:59 -0500

    Mjriveraatindigitall

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,32 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
+    mode: 'development', // Set the mode to 'development' or 'production'
     entry: './src/scripts/app.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: 'src/styles', to: 'styles' },
+           //     { from: 'src/assets', to: 'assets' }, // Adjusted to match your structure
+            ],
+        }),
+    ],
     module: {
         rules: [
             {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
             },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[path][name].[ext]',
-                        },
-                    },
-                ],
-            },
         ],
     },
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,
-        port: 9000,
-    },
-    mode: 'development',
 };
